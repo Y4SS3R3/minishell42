6934079f2a69407c7c8db1756394f338d701eea0
Mahmoud Zouine <mzouine@e3r10p2.1337.ma>
2024-09-07 16:46:33 +0100

    mahmoud

--- parsing/mz_parser.c
+++ parsing/mz_parser.c
@@ -6,7 +6,7 @@
 /*   By: mzouine <mzouine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:54:01 by mzouine           #+#    #+#             */
-/*   Updated: 2024/09/07 16:26:58 by mzouine          ###   ########.fr       */
+/*   Updated: 2024/09/07 16:42:11 by mzouine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,20 +40,20 @@ t_token	*mz_parser(char *s, int *status)
 
 	if (mz_syntax_err(s) == 1)
 	{
-		printf("1\n");
+		// printf("1\n");
 		(*status) = 258;
 		return (NULL);
 	}
 	if (mz_syntax_err2(s) == 1)
 	{
-		printf("2\n");
+		// printf("2\n");
 		(*status) = 258;
 		return (NULL);
 	}
 	head = mz_first_scan(s);
 	if (mz_syntax_handler(head) == -1)
 	{
-		printf("3\n");
+		// printf("3\n");
 		(*status) = 258;
 		return (NULL);
 	}
