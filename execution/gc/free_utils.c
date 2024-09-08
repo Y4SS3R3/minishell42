83a05ad6921a6e226a2ff88c4de18c1e9a9d8344
Yasser Massioui <ymassiou@e3r10p16.1337.ma>
2024-09-08 15:57:44 +0100

    before integrating garbage collector chez mzouine

--- execution/gc/free_utils.c
+++ execution/gc/free_utils.c
@@ -6,7 +6,7 @@
 /*   By: ymassiou <ymassiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 17:03:40 by ymassiou          #+#    #+#             */
-/*   Updated: 2024/09/08 11:33:24 by ymassiou         ###   ########.fr       */
+/*   Updated: 2024/09/08 15:40:58 by ymassiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,12 @@
 void	free_token(t_token *head)
 {
 	t_token	*tmp;
-	int		i;
 
-	i = 0;
 	while (head)
 	{
 		tmp = head;
 		head = head->next;
 		free(tmp->cmd);
-		while (tmp->args && tmp->args[i])
-		{
-			free(tmp->args[i]);
-			i++;
-		}
-		free(tmp->args);
 		free(tmp);
 		tmp = NULL;
 	}
